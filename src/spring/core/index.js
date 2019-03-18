541a8b5155ef663c42092448f379e7174f2657fb
renzhsh <renzhsh@126.com>
2019-03-18 17:41:43 +0800

    core模块导出context

--- src/spring/core/index.js
+++ src/spring/core/index.js
@@ -1,10 +1,13 @@
-import { SpringX } from './springx';
-import RouterX from './routerx';
-import StoreX from './storex';
-import Storage from './storage';
+import { SpringX } from "./springx";
+import RouterX from "./routerx";
+import StoreX from "./storex";
+import Storage from "./storage";
+import context from "./context";
 
 SpringX.use(Storage);
 SpringX.use(RouterX);
 SpringX.use(StoreX);
 
 export default SpringX;
+
+export { context };
