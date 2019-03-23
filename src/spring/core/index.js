97efe24f75fde7e88e1cd59d98501b363a3c3fce
renzhsh <renzhsh@126.com>
2019-03-23 08:11:11 +0800

    调整Storage的使用方式

--- src/spring/core/index.js
+++ src/spring/core/index.js
@@ -1,10 +1,8 @@
 import { SpringX } from "./springx";
 import RouterX from "./routerx";
 import StoreX from "./storex";
-import Storage from "./storage";
 import context from "./context";
 
-SpringX.use(Storage);
 SpringX.use(RouterX);
 SpringX.use(StoreX);
 
--- src/spring/index.js
+++ src/spring/index.js
@@ -1,10 +1,15 @@
-import SpringX from './core';
-import OAuth2 from './oauth2';
-import Interceptor from './interceptor';
-import UserData from './userData';
+import SpringX from "./core";
+import Storage from "./storage";
+import { StorageFactory } from "./storage";
+import OAuth2 from "./oauth2";
+import Interceptor from "./interceptor";
+import UserData from "./userData";
 
-SpringX.use(OAuth2)
-SpringX.use(Interceptor)
-SpringX.use(UserData)
+SpringX.use(Storage);
+SpringX.use(OAuth2);
+SpringX.use(Interceptor);
+SpringX.use(UserData);
 
 export default SpringX;
+
+export { StorageFactory };
