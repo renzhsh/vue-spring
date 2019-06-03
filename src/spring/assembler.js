f9d0868c9bbde613b854a936769c95815866ba27
renzhsh <renzhsh@126.com>
2019-06-03 11:36:02 +0800

    模块组装统一放置在assembler.js文件

--- src/spring/assembler.js
+++ src/spring/assembler.js
@@ -2,4 +2,25 @@
  * SpringX 装配器
  */
 
-export default class Assembler {}
+import { SpringX } from "./springx";
+
+// base
+import Storage from "@spring/base/storage";
+
+// core
+import RouterX from "@spring/core/routerx";
+import StoreX from "@spring/core/storex";
+
+// functional
+import Interceptor from "@spring/functional/interceptor";
+import UserData from "@spring/functional/userData";
+import Auth2 from "@spring/functional/auth";
+
+SpringX.use(Storage);
+
+SpringX.use(RouterX);
+SpringX.use(StoreX);
+
+SpringX.use(Interceptor);
+SpringX.use(UserData);
+SpringX.use(Auth2);
--- src/spring/index.js
+++ src/spring/index.js
@@ -1,18 +1,6 @@
-import SpringX from "./core";
-import Storage from "./base/storage";
-import { StorageFactory } from "./base";
-import Interceptor from "./functional/interceptor";
-import UserData from "./functional/userData";
-import { context } from "./core";
-import Auth2 from './functional/auth';
+import '@/spring/assembler';
+export * from '@spring/base';
 
-SpringX.use(Storage);
-SpringX.use(Interceptor);
-SpringX.use(UserData);
-SpringX.use(Auth2);
+import { SpringX } from "./springx";
 
 export default SpringX;
-
-export { StorageFactory };
-
-export { context as SpringContext };
