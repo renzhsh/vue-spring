5a627232a5b98c5c7de7320bc9f25e1939c72c9d
Sheng <renzhsh@126.com>
2020-05-20 17:04:53 +0800

    修改storage

--- src/spring/base/storage/index.js
+++ src/spring/base/storage/index.js
@@ -1,11 +1,10 @@
-import { MemoryStorage } from "./MemoryStorage";
 import { WebStorage } from "./WebStorage";
 
 // eslint-disable-next-line
 const _global = typeof window !== "undefined" ? window : global || {};
 
 export function StorageFactory({ namespace = "vuejs__", storage = "local" }) {
-    if (storage && ["memory", "local", "session"].indexOf(storage) === -1) {
+    if (storage && ["local", "session"].indexOf(storage) === -1) {
         throw new Error(`Vue-ls: Storage "${storage}" is not supported`);
     }
 
@@ -19,19 +18,6 @@ export function StorageFactory({ namespace = "vuejs__", storage = "local" }) {
         case "session":
             store = "sessionStorage" in _global ? _global.sessionStorage : null;
             break;
-        case "memory":
-            store = MemoryStorage;
-            break;
-    }
-
-    if (!store) {
-        store = MemoryStorage;
-        // eslint-disable-next-line
-        console.error(
-            `Vue-ls: Storage "${
-                _options.storage
-            }" is not supported your system, use memory storage`
-        );
     }
 
     const ws = new WebStorage(store);
@@ -47,44 +33,11 @@ export function StorageFactory({ namespace = "vuejs__", storage = "local" }) {
     return ws;
 }
 
-const ls = StorageFactory({ storage: "local" });
-const ss = StorageFactory({ storage: "session" });
-
-export { ls as _ls };
-export { ss as _ss };
+export const localStorage = StorageFactory({ storage: "local" });
+export const sessionStorage = StorageFactory({ storage: "session" });
 
 export default {
-    /**
-     *
-     */
-    install(SpringX, Vue, useFn, startFn, context) {
-        Vue.$ls = ls;
-        Vue.$ss = ss;
 
-        Object.defineProperties(Vue.prototype, {
-            /**
-             * Define $ls property
-             *
-             * @return {WebStorage}
-             */
-            $ls: {
-                get: function get() {
-                    return ls;
-                }
-            }
-        });
+    install() { }
 
-        Object.defineProperties(Vue.prototype, {
-            /**
-             * Define $ls property
-             *
-             * @return {WebStorage}
-             */
-            $ss: {
-                get: function get() {
-                    return ss;
-                }
-            }
-        });
-    }
 };
